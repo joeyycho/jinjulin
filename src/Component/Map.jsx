f099b6bc33ee6fdfc70d3939caaaa2a38dd5b92d
joeyycho <joeycho@cau.ac.kr>
2025-02-05 11:40:40 +0900

    FEAT: pin color

--- src/Component/Map.jsx
+++ src/Component/Map.jsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import useGeolocation from "../hooks/useGeolocation.js";
 
 function Map() {
@@ -9,12 +9,10 @@ function Map() {
   // API에서 주소 목록 가져오기
   useEffect(() => {
     async function fetchData() {
-      const result = await fetch(process.env.REACT_APP_ADDRESS_API); // API URL
+      const result = await fetch(process.env.REACT_APP_ADDRESS_API);
       const data = await result.json();
       setAddress(data);
-      // console.log('Fetched addresses:', data); // 데이터 확인
     }
-
     fetchData();
   }, []);
 
@@ -38,27 +36,37 @@ function Map() {
 
       mapRef.current = new naver.maps.Map("map", mapOptions);
 
-      // Clear any existing markers before adding new ones
-      const markers = [];
+      const colorMap = {
+        식당: "orange",
+        카페: "yellow",
+        술집: "green",
+        놀거리: "blue",
+        명소: "purple",
+      };
 
-      address.forEach((addressItem, index) => {
+      address.forEach((addressItem) => {
+        const markerColor = colorMap[addressItem.placeType] || "red"; // 기본값 빨강
         const marker = new naver.maps.Marker({
           position: new naver.maps.LatLng(addressItem.lat, addressItem.lng),
           map: mapRef.current,
+          icon: {
+            url: `https://maps.google.com/mapfiles/ms/icons/${markerColor}-dot.png`,
+            size: new naver.maps.Size(30, 36),
+            scaledSize: new naver.maps.Size(30, 36),
+            anchor: new naver.maps.Point(12, 36),
+          },
         });
 
         const infoWindow = new naver.maps.InfoWindow({
           content: `
             <div style="padding:10px 10px 0px 10px; font-weight:bold;">
               ${addressItem.placeName}
-          </div>
+            </div>
             <div style="padding:0px 10px 10px 10px; text-align:center;">
               ${addressItem.placeType}
             </div>
           `,
         });
-
-        markers.push(marker);
 
         naver.maps.Event.addListener(marker, "click", () => {
           if (infoWindow.getMap()) {
